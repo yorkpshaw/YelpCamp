737102af735f48c5a0c199a33fd71f807eb8a835
York Shaw <york.p.shaw@gmail.com>
2023-03-27 13:09:34 -0500

    Updated seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,7 @@
 /* Connect to Mongoose Using Your Models */
 const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -13,15 +15,43 @@ db.once("open", () => {
     console.log("Database Connected");
 });
 
-/* Testing */
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    const c = new Campground({ title: 'purple field' });
-    await c.save();
-    const campgrounds = await Campground.find({});
-    console.log(campgrounds);
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
+/* Code That Did Not Work */
+/* const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+} */
+
+/* Code That Did Work */
+const seedDB = async () => {
+    try {
+        await Campground.deleteMany({});
+        console.log("Deleted all campgrounds");
+        for (let i = 0; i < 50; i++){
+            const random1000 = Math.floor(Math.random() * 1000);
+            const camp = new Campground({
+                location: `${cities[random1000].city}, ${cities[random1000].state}`,
+                title: `${sample(descriptors)} ${sample(places)}`
+            });
+            console.log(camp); // log the camp object being saved
+            await camp.save();
+        }
+    } catch (err) {
+        console.error(err); // log any errors that occur
+    }
 }
+
+/* seedDB().then(() => {
+    mongoose.connection.close();
+}) */
 
 // Execute the above function
 seedDB();
