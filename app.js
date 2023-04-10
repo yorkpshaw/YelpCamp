b2e185dead80b878693ca86ae2ce816e512ec5aa
York Shaw <york.p.shaw@gmail.com>
2023-04-09 20:51:37 -0500

    added stack trace

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/expressError');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
@@ -46,6 +47,7 @@ app.get('/campgrounds/new', (req, res) => {
 
 /* Must parse the req body to view post */
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -73,8 +75,16 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+
+/* This must go at bottom in order to let everything else run first */
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-    res.send('Something went wrong');
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Uh oh something went wrong';
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
