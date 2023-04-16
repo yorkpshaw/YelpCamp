bd3236ebad36e7acea3789c34de3bfee942513aa
York Shaw <york.p.shaw@gmail.com>
2023-04-16 13:07:01 -0500

    added flash success and errors

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -53,6 +53,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
@@ -60,6 +61,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
     res.redirect('/campgrounds');
 }));
 
