3d2d069c0440cb9c2574ffbe533d6aaa12509535
York Shaw <york.p.shaw@gmail.com>
2023-04-16 11:04:15 -0500

    campground routes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const methodOverride = require('method-override');
 const Review = require('./models/review');
 const mongoose = require('mongoose');
 
+const campgrounds = require('./routes/campgrounds');
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     // useCreateIndex: true, <--- Causes app to crash now
@@ -34,15 +36,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 /* Middleware for review */
 const validateReview = (req, res, next) => {
@@ -54,47 +47,12 @@ const validateReview = (req, res, next) => {
         next();
     }
 }
+
+app.use('/campgrounds', campgrounds)
+
 app.get('/', (req, res) => {
     res.render('home')
 })
-
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    // Get the campground data, what you do with it is in the ejs file
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}));
-
-/* Place this before show, or it gets mistake as :id */
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-
-/* Must parse the req body to view post */
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    // Validate Data before it reaches Mongoose
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    // Node.js object that allows you to access the value of a URL parameter
-    // const campground = await Campground.findById(req.params.id).populate({ path: 'reviews', options: { strictPopulate: false } });
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show', { campground });
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
-}));
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
 
 /* Find a specific campground to leave a review for */
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
@@ -105,12 +63,6 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
 
 /* $pull will remove the reference to the review in the campground and the review itself */
 app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
---
+++ routes/campgrounds.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/expressError');
+const Campground = require('../models/campground');
+const { campgroundSchema } = require('../schemas.js');
+
+
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+router.get('/', catchAsync(async (req, res) => {
+    // Get the campground data, what you do with it is in the ejs file
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+/* Place this before show, or it gets mistake as :id */
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+/* Must parse the req body to view post */
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // Validate Data before it reaches Mongoose
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+    // Node.js object that allows you to access the value of a URL parameter
+    // const campground = await Campground.findById(req.params.id).populate({ path: 'reviews', options: { strictPopulate: false } });
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    res.render('campgrounds/show', { campground });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+}));
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}));
+
+
+module.exports = router;
