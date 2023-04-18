331922fefa218f8bd4f92320196ce185cb76a182
York Shaw <york.p.shaw@gmail.com>
2023-04-18 16:04:15 -0500

    added first part of geocoding

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,10 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
+
 
 module.exports.index = async (req, res) => {
     // Get the campground data, what you do with it is in the ejs file
@@ -12,15 +17,20 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    res.send(geoData.body.features[0].geometry.coordinates);
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     // Validate Data before it reaches Mongoose
-    const campground = new Campground(req.body.campground);
+    // const campground = new Campground(req.body.campground);
     /* Map over the array added to req.files */
-    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
+    // campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    // campground.author = req.user._id;
+    // await campground.save();
+    // req.flash('success', 'Successfully made a new campground!');
+    // res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res,) => {
