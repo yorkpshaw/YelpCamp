c23ca8eb205f57a13a5bcbc4e6871c26652ef8c0
York Shaw <york.p.shaw@gmail.com>
2023-04-18 18:05:44 -0500

    controller/users.js security

--- controllers/users.js
+++ controllers/users.js
@@ -4,20 +4,20 @@ module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 }
 
-module.exports.register = async (req, res) => {
+module.exports.register = async (req, res, next) => {
     try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    /* Sign in user after registration */
-    req.login(registeredUser, err => {
-        if(err) return next(err);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
-    })
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        /* Sign in user after registration */
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
-    req.flash('error', e.message);
-    res.redirect('register');
+        req.flash('error', e.message);
+        res.redirect('register');
     }
 }
 
@@ -26,18 +26,15 @@ module.exports.renderLogin = (req, res) => {
 }
 
 module.exports.login = (req, res) => {
-    req.flash('success', 'Welcome back!');
+    req.flash('success', 'welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
     res.redirect(redirectUrl);
 }
 
 module.exports.logout = (req, res) => {
-    req.logout(function(err) {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
-      req.flash('success', "Goodbye!");
-      res.redirect('/campgrounds');
-    });
-  }
+    req.logout();
+    // req.session.destroy();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
