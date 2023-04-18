ed03fba8bb723875594b57d546ec2f6e1cd93746
York Shaw <york.p.shaw@gmail.com>
2023-04-18 09:50:22 -0500

    refactored campgrounds routes with router.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,21 +6,20 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 
 
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    /* Must parse the req body to view post */
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 /* Place this before show, or it gets mistake as :id */
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-/* Must parse the req body to view post */
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 
 module.exports = router;
