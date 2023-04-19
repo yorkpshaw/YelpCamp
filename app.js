522d445f41e6862d591d29ba1157877e1043575b
York Shaw <york.p.shaw@gmail.com>
2023-04-18 19:01:36 -0500

    mongodb store

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+// const MongoDBStore = require("connect-mongo")
 
-const dbUrl = process.env.DB_URL
-// 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     // useCreateIndex: true, <--- Causes app to crash now
@@ -50,8 +51,19 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
     replaceWith: '_'
-}))
+}));
 
+/*
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+});
+*/
 
 const sessionConfig = {
     name: 'session',
